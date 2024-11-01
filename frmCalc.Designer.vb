09cacfba96d4e787856feb9e33d0463bc9b27ff0
dridi <dridi@AZUSA15>
2024-11-01 13:26:22 +0100

    Ajoutez des fichiers projet.

---
+++ frmCalc.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmCalc
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        txt_affiche = New TextBox()
+        b1 = New Button()
+        b2 = New Button()
+        b5 = New Button()
+        b4 = New Button()
+        b3 = New Button()
+        b8 = New Button()
+        b6 = New Button()
+        b7 = New Button()
+        b0 = New Button()
+        b9 = New Button()
+        moin = New Button()
+        div = New Button()
+        mult = New Button()
+        plus = New Button()
+        Label3 = New Label()
+        Label5 = New Label()
+        bpt = New Button()
+        efface = New Button()
+        egal = New Button()
+        SuspendLayout()
+        ' 
+        ' txt_affiche
+        ' 
+        txt_affiche.AccessibleName = "txt_affiche"
+        txt_affiche.Location = New Point(48, 50)
+        txt_affiche.Name = "txt_affiche"
+        txt_affiche.Size = New Size(663, 27)
+        txt_affiche.TabIndex = 2
+        ' 
+        ' b1
+        ' 
+        b1.AccessibleName = "b1"
+        b1.BackColor = SystemColors.ActiveCaptionText
+        b1.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b1.ForeColor = Color.DarkMagenta
+        b1.Location = New Point(38, 119)
+        b1.Name = "b1"
+        b1.Size = New Size(80, 63)
+        b1.TabIndex = 4
+        b1.Text = "1"
+        b1.UseVisualStyleBackColor = False
+        ' 
+        ' b2
+        ' 
+        b2.AccessibleName = "b2"
+        b2.BackColor = SystemColors.ActiveCaptionText
+        b2.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b2.ForeColor = Color.DarkMagenta
+        b2.Location = New Point(163, 119)
+        b2.Name = "b2"
+        b2.Size = New Size(80, 63)
+        b2.TabIndex = 5
+        b2.Text = "2"
+        b2.UseVisualStyleBackColor = False
+        ' 
+        ' b5
+        ' 
+        b5.AccessibleName = "b5"
+        b5.BackColor = SystemColors.ActiveCaptionText
+        b5.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b5.ForeColor = Color.DarkMagenta
+        b5.Location = New Point(163, 215)
+        b5.Name = "b5"
+        b5.Size = New Size(80, 63)
+        b5.TabIndex = 6
+        b5.Text = "5"
+        b5.UseVisualStyleBackColor = False
+        ' 
+        ' b4
+        ' 
+        b4.AccessibleName = "b4"
+        b4.BackColor = SystemColors.ActiveCaptionText
+        b4.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b4.ForeColor = Color.DarkMagenta
+        b4.Location = New Point(38, 215)
+        b4.Name = "b4"
+        b4.Size = New Size(80, 63)
+        b4.TabIndex = 7
+        b4.Text = "4"
+        b4.UseVisualStyleBackColor = False
+        ' 
+        ' b3
+        ' 
+        b3.AccessibleName = "b3"
+        b3.BackColor = SystemColors.ActiveCaptionText
+        b3.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b3.ForeColor = Color.DarkMagenta
+        b3.Location = New Point(288, 119)
+        b3.Name = "b3"
+        b3.Size = New Size(80, 63)
+        b3.TabIndex = 8
+        b3.Text = "3"
+        b3.UseVisualStyleBackColor = False
+        ' 
+        ' b8
+        ' 
+        b8.AccessibleName = "b8"
+        b8.BackColor = SystemColors.ActiveCaptionText
+        b8.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b8.ForeColor = Color.DarkMagenta
+        b8.Location = New Point(163, 316)
+        b8.Name = "b8"
+        b8.Size = New Size(80, 63)
+        b8.TabIndex = 9
+        b8.Text = "8"
+        b8.UseVisualStyleBackColor = False
+        ' 
+        ' b6
+        ' 
+        b6.AccessibleName = "b6"
+        b6.BackColor = SystemColors.ActiveCaptionText
+        b6.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b6.ForeColor = Color.DarkMagenta
+        b6.Location = New Point(288, 215)
+        b6.Name = "b6"
+        b6.Size = New Size(80, 63)
+        b6.TabIndex = 10
+        b6.Text = "6"
+        b6.UseVisualStyleBackColor = False
+        ' 
+        ' b7
+        ' 
+        b7.AccessibleName = "b7"
+        b7.BackColor = SystemColors.ActiveCaptionText
+        b7.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b7.ForeColor = Color.DarkMagenta
+        b7.Location = New Point(38, 316)
+        b7.Name = "b7"
+        b7.Size = New Size(80, 63)
+        b7.TabIndex = 11
+        b7.Text = "7"
+        b7.UseVisualStyleBackColor = False
+        ' 
+        ' b0
+        ' 
+        b0.AccessibleName = "b0"
+        b0.BackColor = SystemColors.ActiveCaptionText
+        b0.Font = New Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b0.ForeColor = Color.DarkMagenta
+        b0.Location = New Point(38, 396)
+        b0.Name = "b0"
+        b0.Size = New Size(205, 63)
+        b0.TabIndex = 12
+        b0.Text = "0"
+        b0.UseVisualStyleBackColor = False
+        ' 
+        ' b9
+        ' 
+        b9.AccessibleName = "b9"
+        b9.BackColor = SystemColors.ActiveCaptionText
+        b9.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        b9.ForeColor = Color.DarkMagenta
+        b9.Location = New Point(288, 316)
+        b9.Name = "b9"
+        b9.Size = New Size(80, 63)
+        b9.TabIndex = 13
+        b9.Text = "9"
+        b9.UseVisualStyleBackColor = False
+        ' 
+        ' moin
+        ' 
+        moin.AccessibleName = "soustract"
+        moin.BackColor = Color.DarkOrchid
+        moin.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        moin.Location = New Point(397, 249)
+        moin.Name = "moin"
+        moin.Size = New Size(136, 90)
+        moin.TabIndex = 14
+        moin.Text = "-"
+        moin.UseVisualStyleBackColor = False
+        ' 
+        ' div
+        ' 
+        div.AccessibleName = "div"
+        div.BackColor = Color.DarkOrchid
+        div.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        div.Location = New Point(576, 249)
+        div.Name = "div"
+        div.Size = New Size(135, 90)
+        div.TabIndex = 15
+        div.Text = "/"
+        div.UseVisualStyleBackColor = False
+        ' 
+        ' mult
+        ' 
+        mult.AccessibleName = "mult"
+        mult.BackColor = Color.DarkOrchid
+        mult.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        mult.Location = New Point(576, 119)
+        mult.Name = "mult"
+        mult.Size = New Size(135, 99)
+        mult.TabIndex = 16
+        mult.Text = "*"
+        mult.UseVisualStyleBackColor = False
+        ' 
+        ' plus
+        ' 
+        plus.AccessibleName = "somme"
+        plus.BackColor = Color.DarkOrchid
+        plus.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        plus.Location = New Point(397, 119)
+        plus.Name = "plus"
+        plus.Size = New Size(136, 99)
+        plus.TabIndex = 17
+        plus.Text = "+"
+        plus.UseVisualStyleBackColor = False
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(97, 452)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 20)
+        Label3.TabIndex = 18
+        ' 
+        ' Label5
+        ' 
+        Label5.AccessibleName = "label_res"
+        Label5.AutoSize = True
+        Label5.Location = New Point(397, 452)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 20)
+        Label5.TabIndex = 20
+        ' 
+        ' bpt
+        ' 
+        bpt.AccessibleName = "b9"
+        bpt.BackColor = SystemColors.ActiveCaptionText
+        bpt.Font = New Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        bpt.ForeColor = Color.DarkMagenta
+        bpt.Location = New Point(288, 398)
+        bpt.Name = "bpt"
+        bpt.Size = New Size(80, 63)
+        bpt.TabIndex = 21
+        bpt.Text = "."
+        bpt.UseVisualStyleBackColor = False
+        ' 
+        ' efface
+        ' 
+        efface.AccessibleName = "reset"
+        efface.BackColor = Color.DarkOrchid
+        efface.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        efface.Location = New Point(576, 370)
+        efface.Name = "efface"
+        efface.Size = New Size(135, 89)
+        efface.TabIndex = 22
+        efface.Text = "c"
+        efface.UseVisualStyleBackColor = False
+        ' 
+        ' egal
+        ' 
+        egal.AccessibleName = "res"
+        egal.BackColor = Color.DarkOrchid
+        egal.Font = New Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        egal.Location = New Point(397, 370)
+        egal.Name = "egal"
+        egal.Size = New Size(136, 89)
+        egal.TabIndex = 23
+        egal.Text = "="
+        egal.UseVisualStyleBackColor = False
+        ' 
+        ' frmCalc
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 533)
+        Controls.Add(egal)
+        Controls.Add(efface)
+        Controls.Add(bpt)
+        Controls.Add(Label5)
+        Controls.Add(Label3)
+        Controls.Add(plus)
+        Controls.Add(mult)
+        Controls.Add(div)
+        Controls.Add(moin)
+        Controls.Add(b9)
+        Controls.Add(b0)
+        Controls.Add(b7)
+        Controls.Add(b6)
+        Controls.Add(b8)
+        Controls.Add(b3)
+        Controls.Add(b4)
+        Controls.Add(b5)
+        Controls.Add(b2)
+        Controls.Add(b1)
+        Controls.Add(txt_affiche)
+        Name = "frmCalc"
+        Text = "Calculatrice"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents txt_affiche As TextBox
+    Friend WithEvents b1 As Button
+    Friend WithEvents b2 As Button
+    Friend WithEvents b5 As Button
+    Friend WithEvents b4 As Button
+    Friend WithEvents b3 As Button
+    Friend WithEvents b8 As Button
+    Friend WithEvents b6 As Button
+    Friend WithEvents b7 As Button
+    Friend WithEvents b0 As Button
+    Friend WithEvents b9 As Button
+    Friend WithEvents moin As Button
+    Friend WithEvents div As Button
+    Friend WithEvents mult As Button
+    Friend WithEvents plus As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents bpt As Button
+    Friend WithEvents efface As Button
+    Friend WithEvents egal As Button
+
+End Class
---
+++ frmCalc.vb
@@ -0,0 +1,159 @@
+﻿Public Class frmCalc
+    Dim A As String
+    Dim B As String
+    Dim op = ""
+
+    Private Sub b0_Click(sender As Object, e As EventArgs) Handles b0.Click
+        If op = "" Then
+            A = A + "0"
+        Else
+            B = B + "0"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b1_Click(sender As Object, e As EventArgs) Handles b1.Click
+        If op = "" Then
+            A = A + "1"
+        Else
+            B = B + "1"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        If op = "" Then
+            A = A + "2"
+        Else
+            B = B + "2"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b3_Click(sender As Object, e As EventArgs) Handles b3.Click
+        If op = "" Then
+            A = A + "3"
+        Else
+            B = B + "3"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b4_Click(sender As Object, e As EventArgs) Handles b4.Click
+        If op = "" Then
+            A = A + "4"
+        Else
+            B = B + "4"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b5_Click(sender As Object, e As EventArgs) Handles b5.Click
+        If op = "" Then
+            A = A + "5"
+        Else
+            B = B + "5"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b6_Click(sender As Object, e As EventArgs) Handles b6.Click
+        If op = "" Then
+            A = A + "6"
+        Else
+            B = B + "6"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b7_Click(sender As Object, e As EventArgs) Handles b7.Click
+        If op = "" Then
+            A = A + "7"
+        Else
+            B = B + "7"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b8_Click(sender As Object, e As EventArgs) Handles b8.Click
+        If op = "" Then
+            A = A + "8"
+        Else
+            B = B + "8"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub b9_Click(sender As Object, e As EventArgs) Handles b9.Click
+        If op = "" Then
+            A = A + "9"
+        Else
+            B = B + "9"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub plus_Click(sender As Object, e As EventArgs) Handles plus.Click
+        If Not A = "" Then
+            op = "+"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub mult_Click(sender As Object, e As EventArgs) Handles mult.Click
+        If Not A = "" Then
+            op = "*"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub moin_Click(sender As Object, e As EventArgs) Handles moin.Click
+        If Not A = "" Then
+            op = "-"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub div_Click(sender As Object, e As EventArgs) Handles div.Click
+        If Not A = "" Then
+            op = "/"
+        End If
+        txt_affiche.Text = A + " " + op + " " + B
+    End Sub
+
+    Private Sub efface_Click(sender As Object, e As EventArgs) Handles efface.Click
+        txt_affiche.Text = ""
+        A = ""
+        B = ""
+        op = ""
+    End Sub
+
+    Private Sub egal_Click(sender As Object, e As EventArgs) Handles egal.Click
+        Dim test = True
+        Dim res As Double
+        If IsNumeric(A) And IsNumeric(B) And Not op = "" Then
+            Dim va = Integer.Parse(A)
+            Dim vb = Integer.Parse(B)
+            If op = "+" Then
+                res = va + vb
+            ElseIf op = "-" Then
+                res = va - vb
+            ElseIf op = "*" Then
+                res = va * vb
+            ElseIf op = "/" And Not vb = "0" Then
+                res = va / vb
+            Else
+                MessageBox.Show("division par 0")
+                test = False
+            End If
+        Else
+            MessageBox.Show("A et B doivent etre deux entiers")
+            test = False
+
+        End If
+        If test Then
+            txt_affiche.Text = txt_affiche.Text + "=" + res.ToString
+        End If
+
+    End Sub
+End Class
